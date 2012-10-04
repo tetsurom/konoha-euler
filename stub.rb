b3584dc8e121b9157963666c3f24fb3de4c55af6
uchida <uchida.atsushi.8@gmail.com>
2012-10-04 13:16:49 +0900

    fix stub.rb

--- stub.rb
+++ stub.rb
@@ -2,7 +2,7 @@
 require 'nokogiri'
 require 'open-uri'
 (1..396).each { |i|
-	open("problem/p#{i}.k","w") { |wfile|
+	open("problem/p#{sprintf("%04d",i)}.k","w") { |wfile|
 		wfile.write "/*\nhttp://projecteuler.net/problem=#{i}\n"
 		doc = Nokogiri::HTML(open("http://projecteuler.net/problem=#{i}"))
 		wfile.write doc.search("div.problem_content").text.delete("\r")
